043b6ebc82e52dc51e4f1af4261e7edde326bc26
PoKento <kentoch6@gmail.com>
2026-07-21 00:03:49 -0400

    Delete Codigo_ESP32_Condor/lib/GPRS_config.h

--- Codigo_ESP32_Condor/lib/GPRS_config.h
+++
@@ -1,7 +0,0 @@
-#define API_URL "http://ejemplo.com" //URL de la api de base de datos
-#define APN "bam.entelpcs.cl"   //Nombre del punto de acceso. Ej: (Entel: bam.entelpcs.cl)
-
-#define GSM_Pin "1234"          //Pin de acceso. Usualmente 0000 o 1234.  Si no es necesario iniciar sesión comentar esta linea.
-
-#define GPRS_User "Tonina"      //Nombre del usuario del servicio.      Si no es necesario iniciar sesión comentar esta linea.
-#define GPRS_Pass "123456"      //Contraseña                            Si no es necesario iniciar sesión comentar esta linea.  
